06c75a3a73516eb98852bedb6e52d71405a3b64a
ud39 <ud-gaming@gmx.de>
2023-06-16 15:38:41 +0200

    Rewrite the elm example to resemble ToDo List and some basic CSS

--- src/Main.elm
+++ src/Main.elm
@@ -1,32 +1,122 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text, section, main_, nav, header, footer)
-import Html.Events exposing (onClick)
-
-main : Program () Int Msg
-main =
-    Browser.sandbox { init = 0, update = update, view = view }
-
-type Msg = Increment | Decrement
+import Html.Attributes exposing (placeholder, style)
+import Html exposing (Html, button, text, section, main_, ul, li, input, h1, Attribute)
+import Html.Events exposing (onClick, onInput)
 
 
-update : Msg -> Int -> Int
+type alias Item =
+    { id : Int
+    , content : String
+    }
+
+
+type alias Model =
+    { items : List Item
+    , newItem : String
+    , nextId : Int
+    }
+
+
+type Msg
+    = Add
+    | Delete Int
+    | UpdateNewItem String
+    | Clear
+
+
+init : Model
+init =
+    { items = []
+    , newItem = ""
+    , nextId = 1
+    }
+
+
+update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
-            model + 1
+        Add ->
+            let
+                newItem =
+                    { id = model.nextId
+                    , content = model.newItem
+                    }
 
-        Decrement ->
-            model - 1
+                updatedItems =
+                    newItem :: model.items
+            in
+            { model | items = updatedItems, newItem = "", nextId = model.nextId + 1}
+
+        Delete itemId ->
+            { model | items = List.filter (\item -> item.id /= itemId) model.items }
+
+        UpdateNewItem newInput ->
+            { model | newItem = newInput }
+
+        Clear ->
+            {model | items = []}
 
 
-view : Int -> Html Msg
+mainStyle : List (Attribute msg)
+mainStyle = [ 
+      style "border" "red solid 2px"
+    , style "margin" "auto"
+    , style "width" "30vw"
+    , style "color" "black" 
+    , style "display" "flex"
+    , style "justify-content" "center"
+    , style "padding" "1rem"
+    ]
+
+
+listStyle : List (Attribute msg)
+listStyle = [
+      style "display" "flex"
+    , style "flex-direction" "column"
+    , style "list-decoration" "none"
+    , style "padding" "1rem"
+    ]
+
+listItemStyle : List (Attribute msg)
+listItemStyle = [
+      style "display" "flex"
+    , style "flex-direction" "row"
+    , style "border" "gray 1px dotted"
+    , style "justify-content" "space-around"
+    ]
+
+
+
+buttonStyle : List (Attribute msg)
+buttonStyle = []
+
+
+view : Model -> Html Msg
 view model =
-    main_ []
-    [section []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
-    ]
+    main_ mainStyle
+    [ section [style "display" "flex", style "flex-direction" "column", style "gap" "1rem",  style "width" "30vw"]
+            [ h1 [style "align-self" "center", style "text-decoration" "underline dotted"] [ text "ToDo List"]
+            , input [onInput UpdateNewItem
+                , placeholder "Add Item" 
+                , Html.Attributes.value model.newItem]
+                []
+            , button [ onClick Add ] [ text "Add Item" ]
+            , button [ onClick Clear ] [ text "Clear Items"]
+            , ul listStyle (List.map itemToLi model.items)
+            ]
+        ]
+
+
+itemToLi : Item -> Html Msg
+itemToLi item =
+    li listItemStyle
+        [ text item.content
+        , button [ onClick (Delete item.id) ] [ text "Delete" ]
+        ]
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox { init = init, update = update, view = view }
